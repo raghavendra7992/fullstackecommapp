4f84fb346e3ee903c59b947caeeda291426b2304
Raghavendra Singh <103146867+raghavendra7992@users.noreply.github.com>
2023-04-14 02:26:53 +0530

    cookie error resolve

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -2,20 +2,33 @@ const ErrorHandler=require("../uitils/Errorhandler.js");
 const catchAsyncErrors=require("./catchAsyncError.js");
 const jwt=require("jsonwebtoken");
 const User=require("../models/userModel.js");
-const isAuthenticatedUser=catchAsyncErrors(async (req,res,next)=>{
-    const { token } = req.cookies;
+// const isAuthenticatedUser=catchAsyncErrors(async (req,res,next)=>{
+//     const { token } = req.cookies;
 
-    if (!token) {
-      return next(new ErrorHandler("Please Login for access this resource", 401));
-    }
+//     if (!token) {
+//       return next(new ErrorHandler("Please Login for access this resource", 401));
+//     }
   
-    const decodedData = jwt.verify(token, process.env.SECRET_KEY);
+//     const decodedData = jwt.verify(token, process.env.SECRET_KEY);
   
-    req.user = await User.findById(decodedData.id);
+//     req.user = await User.findById(decodedData.id);
   
-    next();
-  });
-  
+//     next();
+//   });
+const isAuthenticatedUser = catchAsyncErrors(async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    return next(new ErrorHandler('Please log in to access this resource', 401));
+  }
+
+  const token = authHeader.split(' ')[1];
+  const decodedData = jwt.verify(token, process.env.SECRET_KEY);
+  req.user = await User.findById(decodedData.id);
+
+  next();
+});
+
   // Admin Roles
   
 const authrizeRoles=(...roles) =>{
